5b58bb36305b6e32df61686b87d6c9604d6bacf8
Artur Ptaszek <artur.ptaszek@gmail.com>
2016-09-01 22:33:54 +0200

    Redux theory almost complete

--- slides/presentation/index.js
+++ slides/presentation/index.js
@@ -37,7 +37,8 @@ const images = {
   hammer: require("../assets/images/hammer.jpg"),
   screwdriver: require("../assets/images/screwdriver.jpg"),
   mapFilterReduce: require("../assets/images/map-filter-reduce-emoji.png"),
-  pipes: require("../assets/images/pipes-analogy.jpg")
+  pipes: require("../assets/images/pipes-analogy.jpg"),
+  reduxFlow: require("../assets/images/redux.jpg")
 };
 
 preloader(images);
@@ -50,7 +51,7 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Spectacle theme={theme}>
-        <Deck transition={["slide"]} transitionDuration={500} progress="none">
+        <Deck transition={["slide"]} transitionDuration={500} progress="number">
           <Slide bgImage={images.cover} notes="tell about that is your opinion dumbass. Some things will be obvious">
             <Heading size={1} fit caps>
               MVU Architecture
@@ -66,6 +67,12 @@ export default class Presentation extends React.Component {
             </Heading>
             <Heading size={6} textAlign="left" textColor="white">
               17.09.2016r.
+            </Heading>
+          </Slide>
+
+          <Slide>
+            <Heading size={1} fit caps>
+              Rules
             </Heading>
           </Slide>
 
@@ -245,6 +252,118 @@ export default class Presentation extends React.Component {
               <Appear><ListItem>First-class function</ListItem></Appear>
               <Appear><ListItem>Higher-order function</ListItem></Appear>
             </List>
+          </Slide>
+
+          <Slide>
+            <Heading size={1} fit caps textColor="white">Our achievement</Heading>
+          </Slide>
+
+          <Slide>
+            <Link href="http://todomvc.com" target="_blank"><Heading size={1} fit caps textColor="white">TODO MVC</Heading></Link>
+          </Slide>
+
+          <Slide notes="">
+            <Heading size={1} fit caps textColor="white">Flux</Heading>
+            <Heading size={1} fit caps textColor="white">Architecture</Heading>
+          </Slide>
+
+          <Slide notes="">
+            <Heading size={1} fit caps textColor="white">REDUX</Heading>
+          </Slide>
+
+          <Slide notes="" bgColor="#f5f4f0">
+            <Image height="600px" src={images.reduxFlow} />
+            <Link href="http://staltz.com/unidirectional-user-interface-architectures.html" target="_blank">http://staltz.com/unidirectional-user-interface-architectures.html</Link>
+          </Slide>
+
+          <Slide notes="I mentioned that state is evil so we need to fight with him.">
+            <Heading size={1} fit caps textColor="white">REDUX</Heading>
+          </Slide>
+
+          <Slide notes="">
+            <Heading size={1} fit caps textColor="white">Redux</Heading>
+            <List>
+              <Appear><ListItem>Elm inspired</ListItem></Appear>
+              <Appear><ListItem>Enforces functional paradigm</ListItem></Appear>
+              <Appear><ListItem>Creator proved to community that enables lot of tooling</ListItem></Appear>
+              <Appear><ListItem>Most popular across flux-like architectures</ListItem></Appear>
+            </List>
+          </Slide>
+
+          <Slide notes="">
+            <Heading size={1} caps textColor="white">Action</Heading>
+            <CodePane
+              textSize="25px"
+              lang="js"
+              source={`
+                var action = {
+                  type: 'ADD_TODO',
+                  todo: {
+                    name: 'Homework for' +
+                          'english class'
+                  }
+                };
+              `}
+            />
+          </Slide>
+
+          <Slide notes="">
+            <Heading size={1} caps textColor="white">State</Heading>
+            <CodePane
+              textSize="25px"
+              lang="js"
+              source={`
+                var state = {
+                  todos: [
+                    { name: 'first'},
+                    { name: 'second'}
+                  ],
+                  isFetching: false,
+                  isError: false
+                };
+              `}
+            />
+          </Slide>
+
+          <Slide notes="">
+            <Heading size={1} fit caps textColor="white">Store</Heading>
+            <List>
+              <Appear><ListItem>Single source of truth</ListItem></Appear>
+              <Appear><ListItem>Read-only</ListItem></Appear>
+              <Appear><ListItem>Runs reducer</ListItem></Appear>
+              <Appear><ListItem>Notifies listener</ListItem></Appear>
+            </List>
+          </Slide>
+
+          <Slide notes="">
+            <Heading size={1} fit caps textColor="white">Reducer</Heading>
+            <List>
+              <Appear><ListItem>Pure function</ListItem></Appear>
+              <Appear><ListItem>Takes state and action</ListItem></Appear>
+              <Appear><ListItem>Returns next state</ListItem></Appear>
+            </List>
+          </Slide>
+
+          <Slide notes="">
+            <Heading size={1} caps textColor="white">Reducer</Heading>
+            <CodePane
+              textSize="25px"
+              lang="js"
+              source={`
+                function reducer(state, action) {
+                  switch(action.type) {
+                    case 'ADD_TODO':
+                      return $.extend({}, state, {
+                          todos: action.todos
+                            .concat([action.todo])
+                        });
+
+                    default:
+                      return state;
+                  }
+                }
+              `}
+            />
           </Slide>
         </Deck>
       </Spectacle>
